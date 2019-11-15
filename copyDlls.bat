3ea6da0af31ca82c82ea9ef6502792d12cfb75c5
krazykaze81 <mike97gt@gmail.com>
2019-11-15 09:44:21 -0500

    added files to debug in Visual Studio

---
+++ copyDlls.bat
@@ -0,0 +1,12 @@
+copy "C:\Users\Michael\Documents\GitHub\wolfpro\Debug\qagame_mp_x86.dll" "C:\Program Files (x86)\Return to Castle Wolfenstein\wolfpro" /Y
+copy "C:\Users\Michael\Documents\GitHub\wolfpro\Debug\cgame_mp_x86.dll" "C:\Program Files (x86)\Return to Castle Wolfenstein\wolfpro" /Y
+copy "C:\Users\Michael\Documents\GitHub\wolfpro\Debug\ui_mp_x86.dll" "C:\Program Files (x86)\Return to Castle Wolfenstein\wolfpro" /Y
+
+copy "C:\Users\Michael\Documents\GitHub\wolfpro\Debug\qagame_mp_x86.dll" "C:\Users\Michael\Documents\GitHub\wolfpro\wolfpro" /Y
+copy "C:\Users\Michael\Documents\GitHub\wolfpro\Debug\cgame_mp_x86.dll" "C:\Users\Michael\Documents\GitHub\wolfpro\wolfpro" /Y
+copy "C:\Users\Michael\Documents\GitHub\wolfpro\Debug\ui_mp_x86.dll" "C:\Users\Michael\Documents\GitHub\wolfpro\wolfpro" /Y
+
+copy "C:\Users\Michael\Documents\GitHub\wolfpro\Debug\qagame_mp_x86.dll" "C:\Users\Michael\Documents\GitHub\wolfpro\assets" /Y
+copy "C:\Users\Michael\Documents\GitHub\wolfpro\Debug\cgame_mp_x86.dll" "C:\Users\Michael\Documents\GitHub\wolfpro\assets" /Y
+copy "C:\Users\Michael\Documents\GitHub\wolfpro\Debug\ui_mp_x86.dll" "C:\Users\Michael\Documents\GitHub\wolfpro\assets" /Y
+
